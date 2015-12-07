a0349b0277f14ce8c25ea0d87503731bddd14db2
Zachary <zkoehn@iastate.edu>
2015-12-06 21:51:27 -0600

    Adding in an example from the internet. This is of a mirror.

--- Scene.js
+++ Scene.js
@@ -3,24 +3,15 @@
 // Edit around lines 165 and 195 to change from reflection to refraction.
 //
 
-// a couple of example cube maps
-//var path = "../images/park/";
-//var path = "../images/sky/";
+//README: This code was adapted from the SkyboxWithReflection example from Steve Kautz.
+//		  Not Changed
+//				- The camera handler
+//		  Changed
+//				- How the Skybox is textured
+
 var path = "./images/portal_images/";
 
 var imageNames = [
-                  //path + "px.jpg",
-                  //path + "nx.jpg",
-                  //path + "py.jpg",
-                  //path + "ny.jpg",
-                  //path + "pz.jpg",
-                  //path + "nz.jpg"
-                  //path + "nz.jpg",
-                  //path + "nz.jpg",
-                  //path + "nz.jpg",
-                  //path + "nz.jpg",
-                  //path + "nz.jpg",
-                  //path + "nz.jpg"
 				  path + "portal_wall_v2.jpg",
 				  path + "portal_wall_v2.jpg",
 				  path + "portal_wall_v2.jpg",
@@ -72,8 +63,6 @@ function cameraControl(c, ch)
     c.translateY(-0.1);
     return true;
   case 'j':
-    // need to do extrinsic rotation about world y axis, so multiply camera's quaternion
-    // on left
     q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  5 * Math.PI / 180);
     q2 = new THREE.Quaternion().copy(c.quaternion);
     c.quaternion.copy(q).multiply(q2);
@@ -84,7 +73,6 @@ function cameraControl(c, ch)
     c.quaternion.copy(q).multiply(q2);
     return true;
   case 'i':
-    // intrinsic rotation about camera's x-axis
     c.rotateX(5 * Math.PI / 180);
     return true;
   case 'k':
@@ -94,7 +82,7 @@ function cameraControl(c, ch)
     c.lookAt(new THREE.Vector3(0, 0, 0));
     return true;
   case 'S':
-    c.fov = Math.min(80, c.fov + 5);
+    c.fov = Math.min(100, c.fov + 5);
     c.updateProjectionMatrix();
     return true;
   case 'W':
@@ -102,9 +90,8 @@ function cameraControl(c, ch)
     c.updateProjectionMatrix();
     return true;
 
-    // alternates for arrow keys
+  // alternates for arrow keys
   case 'J':
-    //this.orbitLeft(5, distance)
     c.translateZ(-distance);
     q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  5 * Math.PI / 180);
     q2 = new THREE.Quaternion().copy(c.quaternion);
@@ -112,7 +99,6 @@ function cameraControl(c, ch)
     c.translateZ(distance)
     return true;
   case 'L':
-    //this.orbitRight(5, distance)  
     c.translateZ(-distance);
     q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  -5 * Math.PI / 180);
     q2 = new THREE.Quaternion().copy(c.quaternion);
@@ -120,13 +106,11 @@ function cameraControl(c, ch)
     c.translateZ(distance)
     return true;
   case 'I':
-    //this.orbitUp(5, distance)      
     c.translateZ(-distance);
     c.rotateX(-5 * Math.PI / 180);
     c.translateZ(distance)
     return true;
   case 'K':
-    //this.orbitDown(5, distance)  
     c.translateZ(-distance);
     c.rotateX(5 * Math.PI / 180);
     c.translateZ(distance)
@@ -164,7 +148,7 @@ function start()
   window.onkeypress = handleKeyPress;
 
   var scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 30, 1.5, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera( 80, 1.5, 0.1, 1000 );
   camera.position.x = 0;
   camera.position.y = 0;
   camera.position.z = 5;
@@ -172,56 +156,16 @@ function start()
   
   var ourCanvas = document.getElementById('theCanvas');
   var renderer = new THREE.WebGLRenderer({canvas: ourCanvas});
-
-  // Loads the six images
-  // Note optional second parameter, this allows the texture to be used for refraction 
-  // (and the texture still works for the skybox).
-  //var ourCubeMap = THREE.ImageUtils.loadTextureCube(imageNames);
-  var ourCubeMap = THREE.ImageUtils.loadTextureCube(imageNames, THREE.CubeRefractionMapping);
-  ourCubeMap.wrapS = ourCubeMap.wrapT = THREE.RepeatWrapping;
-  ourCubeMap.repeat.set(1,1);
-  console.log(ourCubeMap);
   
+  //Creates the skybox (the testing room)
   var wallTexture = new THREE.ImageUtils.loadTexture( path + "portal_wall_v2.jpg" );
   wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
   wallTexture.repeat.set(5,5);
-  var wallMaterial = new THREE.MeshBasicMaterial( { map: wallTexture, side: THREE.BackSide } );
-  
-  // Use a built-in Three.js shader for cube maps
-  var cubeMapShader = THREE.ShaderLib["cube"];
-  cubeMapShader.uniforms[ "tCube" ].value = ourCubeMap;
-  var material = new THREE.ShaderMaterial( {
-      fragmentShader: cubeMapShader.fragmentShader,
-      vertexShader: cubeMapShader.vertexShader,
-      uniforms: cubeMapShader.uniforms,
-      side: THREE.BackSide  // we'll only see the inside of the cube
-  } );
-
-  // Make a big ole cube for the skybox
+  var wallMaterial = new THREE.MeshBasicMaterial( { map: wallTexture, side: THREE.BackSide } );  
   var geometry = new THREE.BoxGeometry( 100, 100, 100 );
-
-  // Create a mesh for the skybox using the cube shader as the material
-  //var cube = new THREE.Mesh( geometry, material );
-  var cube = new THREE.Mesh( geometry.clone(), wallMaterial );
-  
-  // Add it to the scene
+  var cube = new THREE.Mesh( geometry, wallMaterial );
   scene.add( cube );
-
-  // put another object in the scene
-  geometry = new THREE.SphereGeometry(0.75);
-  //geometry = new THREE.TorusKnotGeometry(1, .4, 128, 16);
-
-  // to make it look reflective, set the envMap property
-  // we can also set the reflectivity (default 1.0) and/or refraction ratio (default .98)
-  // (Note: to get refraction need to load texture with THREE.CubeRefractionMapping, see above.)
-  //material = new THREE.MeshBasicMaterial({color : 0xffffff, envMap : ourCubeMap});
-  material = new THREE.MeshBasicMaterial({color : 0xffffff, envMap : ourCubeMap,  refractionRatio : 0.66, reflectivity : .75});
-  material.wireframe = false;
-  var sphere = new THREE.Mesh( geometry, material );
-  scene.add(sphere);
-
-  sphere.matrixAutoUpdate = false;
-
+  
   var render = function () {
     requestAnimationFrame( render );
     var increment = 0.5 * Math.PI / 180.0;
@@ -230,13 +174,10 @@ function start()
       switch(axis)
       {
       case 'x':
-        sphere.applyMatrix(new THREE.Matrix4().makeRotationX(increment));
         break;
       case 'y':
-        sphere.applyMatrix(new THREE.Matrix4().makeRotationY(increment));  
         break;
       case 'z':
-        sphere.applyMatrix(new THREE.Matrix4().makeRotationZ(increment));    
         break;
      default:
       }
